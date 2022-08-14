3a0b5cb9ff7edc4e356a621e0d8b32df0c29b854
박정태 <pjt@aspncloud.com>
2022-08-14 12:01:45 +0900

    string 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -10,4 +12,63 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void q0() {
+        String actual = "1,2";
+        String[] result = actual.split(",");
+        assertThat(result).contains("1","2");
+        assertThat(result).containsExactly("1","2");
+
+        actual = "1";
+        result = actual.split(",");
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1");
+    }
+
+    @Test
+    void q1() {
+        String actual = "(1,2)";
+        actual = actual.substring(1, actual.length()-1);
+
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("String 클래스 charAt 특정문자 인덱스 가져오기")
+    void q2() {
+        String actual = "abc";
+
+        int findIndex = getIndex(actual, 'a');
+        assertThat(findIndex).isEqualTo(0);
+
+        findIndex = getIndex(actual, 'b');
+        assertThat(findIndex).isEqualTo(1);
+
+        findIndex = getIndex(actual, 'c');
+        assertThat(findIndex).isEqualTo(2);
+
+
+    }
+
+    @Test
+    @DisplayName("String 클래스 charAt string index out of bounds exception 테스트")
+    void q3() {
+        String actual = "abc";
+
+        assertThatThrownBy(() -> actual.charAt(3))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+    }
+
+    private int getIndex(String actual, char target) {
+        for (int i=0; i<actual.length(); i++) {
+            if(actual.charAt(i) == target) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
 }
